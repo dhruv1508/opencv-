4ce437e1928ed0c62a03e1c3372e1450f1a7a414
dhruv1508 <56450002+dhruv1508@users.noreply.github.com>
2019-10-11 23:41:02 +0530

    Update resize2.py

--- resize2.py
+++ resize2.py
@@ -8,6 +8,7 @@ Created on Fri Oct  4 05:44:06 2019
 import cv2
 img=cv2.imread("index.png")
 resize_img = cv2.resize(img, (int(img.shape[1]/2), int(img.shape[0]/2)))
+resized_image = cv2.resize(img, (650,500))
 cv2.imshow("image",resize_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
